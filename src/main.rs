97929a46822e203c6861002aad085403f1d581d5
Josh Vacarchuk <jvacarch@gmail.com>
2025-01-13 20:40:31 -0500

    head hurts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+
+    // working with data types- primitives
+    // Integers
+    // can be signed (+/-) or unsigned (+)
+    // i8, i16, i32, i64, i128
+    // u8, y16, u32, u64, u128
+    // these are the bits it can hold
+
+    let x: i32 = -42;
+    let y: u64 = 100;
+    println!("signed int: {}", x);
+    println!("unsigned int: {}", y);
+
+    // floats
+    // f32, and f64, thats it
+
+    let pi: f64 = 3.14;
+    println!("pi: {}", pi);
+
+    // booleans
+    // t or f
+
+    let is_snowing: bool = true;
+    println!("is it snowing? {}", is_snowing);
+
+    // character
+    // char - single unicode character
+
+    let letter: char = 'a';
+    println!("{}", a);
 }
